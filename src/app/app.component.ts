4348da7a42e48882fac7101c4d2c5c5b760d1880
nagvyas <nag.vyas83@gmail.com>
2018-11-02 17:48:49 -0400

    2nd commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myLibrary';
+  title = 'Template Driven Form';
+
+  username :string;
+  email :string;
+  nickname :string;
+  password:string;
+
+  onSubmit(Tform:NgForm){
+
+    if(Tform.valid)
+      {
+        
+    }
+  }
 }
+
